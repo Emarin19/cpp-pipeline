a6fbfe1e3a8830372fe2e07f84dfd58b074bff04
Emarin19 <emarin702740530@gmail.com>
2020-03-14 17:42:50 -0600

    change to main function

--- main.cpp
+++ main.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
+
+//Class prototype
 class Calculator{
 public:
-    double sum(double x, double y ){
-        return x+y;
-    }
-    double subtract(double x, double y){
-        x-y;
-    }
+    Calculator();
+    ~Calculator();
+    double sum(double x, double y);
+    double subtract(double x, double y);
+    double divide(double x, double y);
+    double multiply(double x, double y);
 };
-int main(){
 
+//Class definition
+Calculator::Calculator() {}
+Calculator::~Calculator() {}
+double Calculator::sum(double x, double y) {
+    return x+y;
+}
+double Calculator::subtract(double x, double y) {
+    return x-y;
+}
+double Calculator::divide(double x, double y) {
+    return x/y;
+}
+double Calculator::multiply(double x, double y) {
+    return x*y;
+}
+int main(){
+    Calculator cal;
+    std::cout<<cal.sum(2.0, 3.0)<<std::endl;
 }
