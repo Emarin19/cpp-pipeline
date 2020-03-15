0f992229f9b263cc2b99e7ac5bb5b4ad3c633e3f
Emarin19 <emarin702740530@gmail.com>
2020-03-14 23:25:13 -0600

    add coments 2

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,7 @@ double Calculator::subtract(double x, double y) {
     return x-y;
 }
 double Calculator::divide(double x, double y) {
+    //divide two numbers
     return x/y;
 }
 double Calculator::multiply(double x, double y) {
